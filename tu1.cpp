1a8ebadc5fc06f6dfc3ddc64a7821ccfb61b4536
Shivaji Shill <agrojitshill@gmail.com>
2025-12-25 13:10:08 +0600

    Add tu1.cpp file
    This file contains a basic C++ program that prints "Hello World" to the console.
    It demonstrates the use of std::cout, main function, and basic program structure.
    Suitable for beginners to learn compiling and running a C++ program.

---
+++ tu1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //Include the standard input-output library (need for cout)
+
+int main() // Main function: the entry point of the program
+
+{
+    std::cout << "Hello World";
+    //std::cout prints the text to the console
+    //"Hello World" is the text that will appear on the screen
+    // std:: means we are using 'cout' from the standard namespace
+    // cout = console output → displays text on the screen
+    // "<<" → indicate output
+    // ">>" → indicate input
+
+    return 0;
+    // return 0 indicates that the program ended successfully
+}
