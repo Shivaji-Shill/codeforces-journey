3d50178d80940b2947da7a7c1d0a7c39ee3c08dd
Shivaji Shill <agrojitshill@gmail.com>
2025-12-29 23:33:17 +0600

    Add basic addition program in C++
    This program takes two integers as input and displays their sum.

---
+++ Class_04/class4_addition.cpp
@@ -0,0 +1,3 @@
+This program takes two integer values as input from the user using cin
+and displays their sum using cout. It was written as part of Class 4
+to practice basic input, output, and arithmetic operations in C++.
